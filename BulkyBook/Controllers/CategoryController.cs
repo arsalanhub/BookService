c938569f6fc1668ded48bb87c487eab8bbebc4ef
Mohammad Arsalan <arsalanmohd237@gmail.com>
2022-07-15 16:27:26 +0530

    category view created

--- BulkyBook/Controllers/CategoryController.cs
+++ BulkyBook/Controllers/CategoryController.cs
@@ -17,5 +17,9 @@ namespace BulkyBook.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories;  
             return View(objCategoryList);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
