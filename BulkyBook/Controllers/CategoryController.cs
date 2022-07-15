f6b996937bdd2be70b3a27e762e992387b803d9e
Mohammad Arsalan <arsalanmohd237@gmail.com>
2022-07-15 17:04:14 +0530

    server side validation

--- BulkyBook/Controllers/CategoryController.cs
+++ BulkyBook/Controllers/CategoryController.cs
@@ -27,6 +27,10 @@ namespace BulkyBook.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The DisplayOrder cannot exactly match the Name");
+            }
             if(ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
